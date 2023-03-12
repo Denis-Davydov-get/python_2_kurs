4fd07c26dfdb365129422efbfdc2a58abc06104b
Denis-Davydov-get <slim2001.88@mail.ru>
2023-03-12 08:45:47 +0300

    rewrite code

---
+++ seminar/task1.py
@@ -0,0 +1,4 @@
+import math
+speed_day = 700 #проезжает за день
+rasstoyanie= 750 #растояние
+print("Количество дней за которое нужно проехать - ", math.ceil(rasstoyanie / speed_day))
---
+++ seminar/task3.py
@@ -0,0 +1,3 @@
+students = input("Сколько учеников учится? ")
+part_need = int(students) / 2
+print("Парт нужно для учеников", round(part_need))
